de72694d6fb5fdef41b304fa4e47e2ed88a4cca0
Alejandro Rojas <kmachappy@pm.me>
2022-03-22 03:14:11 -0700

    callback function and select button on html

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
+// const $location = $(".location")
+// const $temperature = $(".temperature") 
+// const $feels = $(".feels")
+// const $condition = $(".condition")
 
-//console.log("working ")
+const apikey = "c2e2c691dca22443281625adb550280f"
 
 const $input = $("input")
 const $button = $("button")
-const $location = $(".location")
-const $temperature = $(".temperature") 
-const $feels = $(".feels")
-const $condition = $(".condition")
-const apikey = "c2e2c691dca22443281625adb550280f"
-const $select = $("select")
-console.log($select)
-
+// span domquery
 const $loc = $("#loc")
 const $temp = $("#temp") 
 const $feel = $("#feel")
 const $con = $("#con")
+const $select = $("select")
+
+ console.log($select)
+
+
+// let apilink = () => `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apikey}`
+//  console.log(apilink)
 
 //console.log($condition)
+// callback
 
-$button.on("click", () => {
-    const city = $input.val()
+// const 
+// make functino for if on value 
+
+
+const weatherGet = () =>{
+    let city = $input.val()
     console.log(city)
     $.ajax(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apikey}`)
     .then((data) =>{
@@ -32,16 +41,27 @@ $button.on("click", () => {
             $loc.text(` ${data.name} `)
             $temp. html(` ${data.main.temp} &#8457;`)
             $feel.html(` ${data.main.feels_like} &#8457;`)
-            $con.text(` ${data.weather[0].description} `)
+            $con.text(` ${(data.weather[0].description).toUpperCase()} `)
         })
         
         
     })
+    //input clear
+    //$input.val("")
+}
 
-    
+
+// click on button does weather get 
+$button.on("click", () => weatherGet())
+// enter does weather get
+$(document).on("keypress", event => {
+    if(event.which == 13) {
+        weatherGet()
+        
+    }
 })
-if (window.navigator.geolocation) {
-    // Geolocation available
-   } 
+
+// if drop down value more than 2 do this other similar function
+
 
 // question how to append text to existing text in a div without using span
